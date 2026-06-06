cba09a360b994d190ba891f5a26420d8d0324bd6
Deepak Singh <abroy802154@gmail.com>
2026-06-06 06:50:10 +0530

    Updated routeTree.gen.ts

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -103,13 +103,3 @@ declare module '@tanstack/react-start' {
     config: Awaited<ReturnType<typeof startInstance.getOptions>>
   }
 }
-
-import type { getRouter } from './router.tsx'
-import type { startInstance } from './start.ts'
-declare module '@tanstack/react-start' {
-  interface Register {
-    ssr: true
-    router: Awaited<ReturnType<typeof getRouter>>
-    config: Awaited<ReturnType<typeof startInstance.getOptions>>
-  }
-}
